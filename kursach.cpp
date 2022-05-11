cae3ab6807df9912b362c277eac1697497a0fdc8
Nikatik <nikita.a.sergeev@mail.ru>
2022-05-11 03:18:33 +0300

    Format

--- kursach.cpp
+++ kursach.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-#include <cstdlib>
-#include <time.h>
 #include "probe.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
 
 using namespace std;
 
@@ -9,23 +9,27 @@ int main()
 {
     double I = 300.0, P = 350.0, z, V, M;
     double g = 1.634, tol3, tol2, tol1;
+
+    long double* t1 = new long double[3];
+    long double* t2 = new long double[3];
+
     bool drop = 0;
-    long double *t1 = new long double[3];
-    long double *t2 = new long double[3];
+
     while (drop != 1)
     {
-        //get info
-        //if (vertical_period == 1)
-            drop = 1;
+        // get info
+        // if (vertical_period == 1)
+        drop = 1;
     }
-	// x1 = z;
+
+    // x1 = z;
     // x2 = V;
     // a = P / (g * I);
-    //x1 = k*a^2/(3*M^2)*t^3+(k*a/(2*M)-g/2)*t^2
-    //x2 = -k*a^3/(3*M^3)*t^3 - k*a^2/(3*M^2)*t^2 + (g*M-k*a)/M*t
-	
+    // x1 = k*a^2/(3*M^2)*t1^3+(k*a/(2*M)-g/2)*t1^2
+    // x2 = -k*a^3/(3*M^3)*t2^3 - k*a^2/(3*M^2)*t2^2 + (g*M-k*a)/M*t2
+    // t1-t2<tol1
+
     cout << "Hello World!\n";
-    
+
     return 0;
 }
-
